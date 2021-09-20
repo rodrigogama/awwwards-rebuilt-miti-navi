b9f88393d92e5e76c07314de9d778babeedf5c05
Rodrigo Gama <ro.gama91@gmail.com>
2021-09-20 19:51:36 -0300

    feat: add initial tailwind custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,48 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: {
+        100: '#232323',
+        500: '#000E13',
+        550: '#001014',
+      },
+      blue: '#b3d4fc',
+      cream: {
+        50: '#e6dece',
+        100: '#e7decf',
+        150: '#E8DED0',
+        500: '#FFDEAD',
+      },
+      white: '#fff',
+    },
+    screens: {
+      lg: { max: '1090px' }, // => @media (max-width: 1090px) { ... }
+    },
+    extend: {
+      fontFamily: {
+        voyage: ['Voyage'],
+        'gt-pressura-mono': ['GTPressuraMono'],
+        'gt-sectra-display': ['GTSectraDisplay'],
+      },
+      scale: {
+        80: '0.8',
+      },
+      transitionDuration: {
+        600: '600ms',
+      },
+      transitionProperty: {
+        top: 'top',
+      },
+      transitionTimingFunction: {
+        miti: 'cubic-bezier(0.82, 0, 0.36, 1)',
+      },
+      width: {
+        logo: '50px',
+      },
+    },
   },
   variants: {
     extend: {},
