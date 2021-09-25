8c7120a7ba5bcef558f0ec86912e8c06ea3cd63e
Rodrigo Gama <ro.gama91@gmail.com>
2021-09-24 22:04:31 -0300

    feat: configure locomotive scroll provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,26 @@
+import React from 'react';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll';
 import '../styles/globals.css';
+import '../styles/locomotive-scroll.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const containerRef = React.useRef(null);
+  const router = useRouter();
+
+  return (
+    <LocomotiveScrollProvider
+      options={{
+        smooth: true,
+      }}
+      watch={[router.asPath]}
+      containerRef={containerRef}
+    >
+      <main data-scroll-container ref={containerRef}>
+        <Component {...pageProps} />
+      </main>
+    </LocomotiveScrollProvider>
+  );
 }
 export default MyApp;
