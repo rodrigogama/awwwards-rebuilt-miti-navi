56cf7b59632d9c45b848a9d76593826b3cb1d15d
Rodrigo Gama <ro.gama91@gmail.com>
2021-09-24 22:04:45 -0300

    wip: main page with header and footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
 import type { NextPage } from 'next';
+// import { useLocomotiveScroll } from 'react-locomotive-scroll';
+import { Header } from '../components/Header';
+import { Footer } from '../components/Footer';
 
 const Home: NextPage = () => {
+  // const { scroll } = useLocomotiveScroll();
+
   return (
     <>
-      <div>Awwwards rebuilt</div>
+      <Header isLoaded />
+      <div
+        id="main"
+        data-scroll-section
+        style={{ margin: '50px 0', height: '100vh' }}
+      >
+        Awwwards rebuilt
+      </div>
+      <Footer />
     </>
   );
 };
